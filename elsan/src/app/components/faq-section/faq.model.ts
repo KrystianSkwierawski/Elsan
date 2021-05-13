01b68f8a3da5261cbf1f9fdce71a3d67ae5622b7
SZEFNUDY <52860350+SZEFNUDY@users.noreply.github.com>
2021-05-13 16:00:47 +0200

    No message

--- elsan/src/app/components/faq-section/faq.model.ts
+++ elsan/src/app/components/faq-section/faq.model.ts
@@ -3,13 +3,3 @@ interface Faq{
     question: string,
     isSelected: boolean
 }
-
-// class faq {
-//     question: string;
-//     answer: string;
-
-//     constructor(question, answer) {
-//         this.question = question;
-//         this.answer = answer;
-//     }
-// } 
